51e30983b78be5a8451683c83beea2b020363803
daebon07 <muhammadshabran25@gmail.com>
2024-10-03 08:06:16 +0700

    Pemilihan2Percobaan1

---
+++ Pemilihan2Percobaan1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Pemilihan2Percobaan1 {
+    public static void main(String[] args) {
+        Scanner input20 = new Scanner (System.in);
+        
+        int tahun;
+
+        System.out.print("masukan tahun");
+        tahun = input20.nextInt();
+
+        if (( tahun % 4) == 0){
+            if (( tahun % 100) != 0)
+            System.out.println("tahun kabisat");
+        }
+        else{
+            System.out.println("bukan tahun kabisat");
+        }
+    }
+}
